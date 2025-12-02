6ad347f0a5f115116ac241a740e15e72963c1ac8
kcaliban <krausef@posteo.net>
2025-12-02 17:52:31 +0100

    Naming

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
-// cant use constexpr as AVR headers expect a #define
-#define F_CPU 16'000'000UL
+// cant use constexpr as AVR headers expect a macro
+#define F_CPU 16'000'000UL  // CPU frequency
 
 #include <avr/io.h>
 #include <util/delay.h>
 
+// On-board LED blinking at 500ms interval
 int main() {
   DDRB |= (1 << PB5);
 
